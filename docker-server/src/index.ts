486ef80f020a7dce53ea079cbc1985ad08cfc416
Aung Myo Aye <aungmyoayedev@gmail.com>
2026-05-29 22:27:55 +0630

    feat : add mongo db image.

--- docker-server/src/index.ts
+++ docker-server/src/index.ts
@@ -1,10 +1,12 @@
 import dotenv from "dotenv";
 import express, { type Request, type Response } from "express";
+import { MongoClient } from "mongodb";
 dotenv.config();
 
 const PORT = process.env.PORT || 4100;
 
 const app = express();
+const db = new MongoClient(process.env.MONGO_URI!);
 app.use("/health", (req: Request, res: Response) => {
   res.json({
     success: true,
@@ -19,4 +21,12 @@ app.use("/", (req: Request, res: Response) => {
   });
 });
 
-app.listen(PORT, () => console.log("Server is running on port " + PORT));
+async function start() {
+  try {
+    await db.connect();
+    app.listen(PORT, () => console.log("Server is running on port " + PORT));
+  } catch (error) {
+    throw new Error("Something worng.");
+  }
+}
+start();
