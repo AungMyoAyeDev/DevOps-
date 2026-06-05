d8001cc20cfea6f1ba39f36f9aa01158bc726332
Aung Myo Aye <aungmyoayedev@gmail.com>
2026-06-05 07:14:06 +0630

    first ci.

--- api/src/users/controller.ts
+++ api/src/users/controller.ts
@@ -3,7 +3,14 @@ import Users from "./model";
 
 export const getUsersController = async (req: Request, res: Response) => {
   // const users = await Users.find();
-  const users = [{ name: "qq", email: "q@q.com" }];
+  const users = [
+    { name: "qq", email: "q@q.com" },
+    { name: "qq", email: "q@q.com" },
+    { name: "qq", email: "q@q.com" },
+    { name: "qq", email: "q@q.com" },
+    { name: "qq", email: "q@q.com" },
+    { name: "qq", email: "q@q.com" },
+  ];
   res.status(200).json({
     success: true,
     message: "Get users successfull.",
