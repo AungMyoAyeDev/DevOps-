d432039cf8a41916da89c9a2244a28bdf10c9730
Aung Myo Aye <aungmyoayedev@gmail.com>
2026-06-10 22:23:04 +0630

    add static file to server,

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
+import { resolve } from "path";
 
-const PORT = "3000";
+dotenv.config();
+const PORT = process.env.PORT || "3000";
 
 const app = express();
 
-app.use("/", (req, res) => {
+app.use("/api", (req, res) => {
   res.status(200).json("server is running");
 });
 
+if (process.env.NODE_ENV === "production") {
+  const staticPath = resolve(process.cwd(), "dist/client");
+  app.use(express.static(staticPath));
+  app.use((req, res, next) => {
+    if (req.method === "GET" && !req.url.startsWith("/api/")) {
+      res.sendFile(resolve(staticPath, "index.html"));
+    } else {
+      next();
+    }
+  });
+}
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
 });
